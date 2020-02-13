f8732ae94e331da8313ef78670804dddc8756307
Etherll <61019402+Etherll@users.noreply.github.com>
2020-02-13 18:47:07 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# OKanime
-python wrapper for OKanime
+
+
+
+Dependencies
+============
+
+- python 3.*
+- requests
+- bs4
+
+Installation
+============
+
+pip install ok-anime
+
+example
+==========
+
+    from ok_anime import Anime
+    ok = Anime('Anime Name')
+    print(ok.title)
+
+index:	
+	#json of all searchs in OKAnime for anime name
+	print(ok.search)
+	#get anime title
+    	print(ok.title)
+	#get anime genre
+	print(ok.genre)
+	#get anime url
+	print(ok.url)
+	#get anime cover
+	print(ok.cover)
+	#get anime status
+	print(ok.status)
+	#get anime year
+	print(ok.year)
+	#get anime episodes
+	print(ok.episodes)
+	#get anime age_classification
+	print(ok.age_classification)
+	print(ok.rate)
+	#get anime rate
+	print(ok.description)
+	#get anime
+	print(ok.trailer)
+	#get anime
+	print(ok.studio)
+	#get anime
+	print(ok.director)
+	#get anime
+	
+
+this information is fetched from OkAnime
+
+Make sure you don't spam the tests too quickly! One of the tests involves POSTing invalid credentials to OKAnime, so you're likely to be IP-banned if you do this too much in too short a span of time.
---
+++ setup.py
@@ -0,0 +1,31 @@
+try:
+  from setuptools import setup
+except ImportError:
+  from distutils.core import setup
+import setuptools
+
+
+long_description = open('README.md').read()
+
+
+config = {
+  'name': 'ok-anime',
+  'description': 'this is a python wrapper for https://www.okanime.com',
+  'author': 'Etherl',
+  'keywords': ['ok anime', 'anime', 'ok','Arabic'],
+  'python_requires': '>=3.4, <4',
+  'license': '''Copyright (c) 2019 The Python Packaging Authority
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+.''',
+  'long_description': long_description,
+  'long_description_content_type': 'text/markdown',
+  'url': 'https://github.com/Etherl/OkAnime',
+  'version': '1.0',
+  'install_requires': ['requests', 'bs4'],
+  'packages': setuptools.find_packages()
+}
